8e9030f50095bf29803b6902f68a5b5399c0aab7
alexzou999 <142867203+alexzou999@users.noreply.github.com>
2024-02-27 18:01:13 +0000

    Tue, Feb 27, 2024, 10:01 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -2,13 +2,17 @@ loadText = document.querySelector('.loading-text')
 bg = document.querySelector('.bg')
 
 load = 0;
-blurring()
 
-loadText.style.opacity = 0.77
-bg.style.filter = "blur(10px)"
+int = setInterval(blurring, 33)
+
 
 function blurring() {
     load++
+    if (load > 99){
+        clearInterval(int)
+    }
+    
     loadText.innerText = `${load}%`
-
+    loadText.style.opacity = 1
+    bg.style.filter = "blur(10px)"
 }
