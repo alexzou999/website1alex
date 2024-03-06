cb605745db11a3cdbd39a41f3fd347817a652b0c
alexzou999 <142867203+alexzou999@users.noreply.github.com>
2024-03-06 17:11:43 +0000

    Wed, Mar 6, 2024, 9:11 AM -08:00

--- js/navigation.js
+++ js/navigation.js
@@ -6,7 +6,7 @@ window.addEventListener('scroll', fixNav)
 
 function fixNav() {
     console.log(window.scrollY)
-    if(window.scrollY > 57) {
+    if(window.scrollY > 223) {
         nav.classList.add('active')
     } else {
         nav.classList.remove('active')
