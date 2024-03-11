337375bf0b3e3442b9a21c9b378e190b07775d42
alexzou999 <142867203+alexzou999@users.noreply.github.com>
2024-03-11 16:55:56 +0000

    Mon, Mar 11, 2024, 9:55 AM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,5 +1,11 @@
 toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+model.document.getElementById('model')
 
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-model')
 })
