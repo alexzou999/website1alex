a75ed5599b0bbc3c4bca5f4439edfb41ca5e8e40
alexzou999 <142867203+alexzou999@users.noreply.github.com>
2024-03-11 17:10:57 +0000

    Mon, Mar 11, 2024, 10:10 AM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,16 +1,23 @@
 toggle = document.getElementById('toggle')
 open = document.getElementById('open')
 modal = document.getElementById('modal')
-close =
-
+close = document.getElementById('close')
+// Toggle Nav
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
-
+// Show Modal
 open.addEventListener('click', () => {
     modal.classList.add('show-modal')
 })
-
+//Hide Modal
 close.addEventListener('click', () => {
     modal.classList.remove('show-modal')
+})
+
+//Hide Modal on outside click
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
 })
