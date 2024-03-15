fe11fd4322783b9d1b3e6da4b17505b50d1281d7
alexzou999 <142867203+alexzou999@users.noreply.github.com>
2024-03-15 17:01:20 +0000

    Fri, Mar 15, 2024, 10:01 AM -07:00

--- js/search.js
+++ js/search.js
@@ -1,6 +1,10 @@
 btn = document.querySelector('.btn')
 search = document.querySelector('.search')
+toggle = document.getElementById('toggle')
 
 btn.addEventListener('click', () => {
     search.classList.toggle('active')
+})
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav')
 })
